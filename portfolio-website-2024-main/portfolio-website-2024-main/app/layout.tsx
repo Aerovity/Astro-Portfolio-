b37010d6581f7000ffb84642c0dc1625a6844249
Aerovity <zizoubrahmi7@gmail.com>
2025-07-16 00:51:25 +0200

    oui i forgot

---
+++ portfolio-website-2024-main/portfolio-website-2024-main/app/layout.tsx
@@ -0,0 +1,96 @@
+import "./globals.css";
+import React, { ReactNode } from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const syne = Inter({
+  subsets: ["latin"],
+  display: "block",
+  weight: ["400", "500", "600", "700", "800"],
+});
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://www.example.com/"),
+  title: "Abdelaziz Brahmi - Portfolio",
+  description: "Zizou's portfolio website, showcasing my work and skills.",
+  generator: "Next.js",
+  applicationName: "Portfolio template",
+  keywords: [
+    "Portfolio template",
+    "React",
+    "developer",
+    "backend",
+    "nextjs",
+    "react",
+    "rust",
+    "typescript",
+    "cplusplus",
+    "tailwindcss",
+    "java",
+    "meta",
+    "facebook",
+    "backend developer",
+    "backend engineer",
+    "creative",
+    "creative developer",
+    "creative engineer",
+    "tech",
+    "software",
+    "software developer",
+    "portfolio",
+    "software developer portfolio",
+    "backend engineer portfolio",
+    "software engineer portfolio",
+    "developer portfolio",
+    "AI portfolio",
+    "AI developer",
+    "AI engineer",
+    "AI",
+    "artificial intelligence",
+    "machine learning",
+    "deep learning",
+    "computer vision",
+    "NLP",
+    "natural language processing",
+    "data science",
+    "data analysis",
+    "data visualization",
+    "data engineering",
+    "data",
+    "data engineer",
+    "data scientist",
+    "data analyst",
+    "data visualization engineer",
+  ],
+  colorScheme: "dark",
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  category: "technology",
+};
+
+type RootLayoutProps = {
+  children: ReactNode;
+};
+
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <html lang="en">
+      <body
+        className={`${syne.className} scroll-smooth scrollbar-none scrollbar-track-[#0E1016] scrollbar-thumb-[#212531]`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
